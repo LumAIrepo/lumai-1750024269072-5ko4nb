8833c16de30680e7be418c8d3215e049f8ccc6e3
LumAI <zurichixbt@gmail.com>
2025-06-15 23:51:18 +0200

    Add lib/utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,21 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatAddress(address: string, chars = 4): string {
+  return `${address.slice(0, chars)}...${address.slice(-chars)}`
+}
+
+export function formatSOL(amount: number): string {
+  return `${amount.toFixed(4)} SOL`
+}
+
+export function formatNumber(num: number): string {
+  if (num >= 1e9) return (num / 1e9).toFixed(1) + 'B'
+  if (num >= 1e6) return (num / 1e6).toFixed(1) + 'M'
+  if (num >= 1e3) return (num / 1e3).toFixed(1) + 'K'
+  return num.toString()
+}
